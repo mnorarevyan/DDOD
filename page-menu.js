87e1797c4a337970919e07f54fbc633d41b0b9ed
Maria Nor-Arevyan <mnorarevyan@gmail.com>
2022-12-25 20:22:24 -0500

    Make page menu display and hide content accordingly as you click through options

--- page-menu.js
+++ page-menu.js
@@ -8,5 +8,12 @@ for (let pageMenuItem of pageMenuItems) {
             pageMenuItem.classList.remove("active");
         }
         pageMenuItem.classList.add("active");
+
+        const displayContent = document.querySelectorAll(`.${pageMenuItem.classList[1]}`);
+        const contentOptions = document.querySelectorAll(".content-option");
+        for (let contentOption of contentOptions) {
+            contentOption.classList.remove("active");
+        }
+        displayContent[1].classList.add("active");
     });
 }
