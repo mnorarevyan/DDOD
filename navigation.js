2a7d873a709dbb1b8451788d4ba39bfe7353b5ce
Maria Nor-Arevyan <mnorarevyan@gmail.com>
2023-01-01 22:10:20 -0500

    Add 'About Us' page and add functionality make make the dropdown work for the 'About Us' dropdown

--- navigation.js
+++ navigation.js
@@ -17,11 +17,19 @@ function makeLinkActive() {
     displayContent[2].classList.add("active");
 }
 
-const dropdownItemLinks = document.querySelectorAll(".dropdown-1");
-for (let link of dropdownItemLinks) {
+const dropdown1 = document.querySelectorAll(".dropdown-1");
+for (let link of dropdown1) {
     link.addEventListener("click", () => {
         // Remember which dropdown option the user selected for this dropdown
         sessionStorage.setItem("dropdown-1", link.classList[2]);
+    });
+}
+
+const dropdown2 = document.querySelectorAll(".dropdown-2");
+for (let link of dropdown2) {
+    link.addEventListener("click", () => {
+        // Remember which dropdown option the user selected for this dropdown
+        sessionStorage.setItem("dropdown-2", link.classList[2]);
     });
 }
 
@@ -40,5 +48,22 @@ if (sessionStorage.getItem("dropdown-1")) {
     displayContent[2].classList.add("active");
 
     sessionStorage.removeItem("dropdown-1");
+}
+
+if (sessionStorage.getItem("dropdown-2")) {
+    let dropdownOption2 = sessionStorage.getItem("dropdown-2");
+    const toActivate = document.querySelectorAll(`.${dropdownOption2}`)[1];
+    for (let pageMenuItem of pageMenuItems) {
+        pageMenuItem.classList.remove("active");
+    }
+    toActivate.classList.add("active");
+    const displayContent = document.querySelectorAll(`.${toActivate.classList[1]}`);
+    const contentOptions = document.querySelectorAll(".content-option");
+    for (let contentOption of contentOptions) {
+        contentOption.classList.remove("active");
+    }
+    displayContent[2].classList.add("active");
+
+    sessionStorage.removeItem("dropdown-2");
 }
 
